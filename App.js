d4219db71b06d7b17b900f76bc8310b0c0b1089f
pjay79 <praveen.jayarajan@gmail.com>
2018-06-20 09:36:17 +1000

    Fix SplashScreen

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-import React, { Component } from 'react';
-import SplashScreen from 'react-native-splash-screen';
+import React from 'react';
 import MainNavigator from './app/routes/MainNavigator';
 
-export default class App extends Component {
-  componentDidMount() {
-    SplashScreen.hide();
-  }
+const App = () => <MainNavigator />;
 
-  render() {
-    return <MainNavigator />;
-  }
-}
+export default App;
